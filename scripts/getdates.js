336e7987e04e51b1ab80344f50f2784bf7d5bf1d
Tomzyofficial <tomzyofficial54@gmail.com>
2024-11-09 17:01:48 +0100

    week-1-home-page

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+const d = new Date();
+const fullyear = d.getFullYear();
+
+document.getElementById("currentyear").innerHTML = fullyear;
+document.getElementById("lastModified").innerHTML = document.lastModified;
