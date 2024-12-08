11bfb3c567e21b93ba7c5aa61473a6d555882f3c
Tomzyofficial <tomzyofficial54@gmail.com>
2024-12-08 01:55:26 +0100

    form-submission-fix

--- scripts/submission.js
+++ scripts/submission.js
@@ -1,17 +1,10 @@
 // form submission
 document.addEventListener("DOMContentLoaded", () => {
-  let localStoredKey = "Review counter";
-  let storedKey = localStorage.getItem(localStoredKey);
-  let res = storedKey ? parseInt(storedKey, 10) : 0;
-  res++;
-  localStorage.setItem(localStoredKey, res);
-  const msg = document.querySelector("#counter");
-
-  // check if review is greater than 1 add "s";
-  if (res == 1) {
-    msg.innerHTML = `Congratulation! you have submitted ${res} review`;
-  } else {
-    msg.innerHTML = `Congratulation! you have submitted ${res} reviews`;
-  }
+  let storedKey = localStorage.getItem("Review counter");
+  let counter = storedKey ? parseInt(storedKey, 10) : 0;
+  counter++;
+  localStorage.setItem("Review counter", counter);
+  const msg = document.querySelector("#counter-msg");
+  msg.innerHTML = `Congratulation! your review is ${counter} total`;
 
 })
